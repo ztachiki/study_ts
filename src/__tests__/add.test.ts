80c3bbfbf9d658f77f5f95be816439199d9defe4
ztachiki <ztachiki@gmail.com>
2021-05-07 19:30:49 +0900

    update

--- src/__tests__/add.test.ts
+++ src/__tests__/add.test.ts
@@ -1,8 +1,8 @@
-import add from "../add";
+import  add  from "../add";
 
-describe('add', (): void => {
-  test('should return 3', (): void => {
-    const result: number = add(1, 2);
+describe('add', () => {
+  test('should return 3', () => {
+    const result = add(1, 2);
     expect(result).toEqual(3);
   });
 });
